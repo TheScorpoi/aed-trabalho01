83b2ecb73001be3b2dc9524219dba09dc2b2756a
TheScorpoi <sobral@ua.pt>
2020-12-27 01:00:51 +0000

    resultados 98629

--- Resultados/graficoTempos.m
+++ Resultados/graficoTempos.m
@@ -1,31 +1,33 @@
-clear all;
-close all;
+clear all; close all;
 
-file = load("temposExecucao.txt")
+% Gráfico com os tempos de execução
 
-%file2 = load("temposExecucao.txt");
-%file3 = load("temposExecucao.txt");
+file98491 = load("temposExecucao98491.txt");
+file98495 = load("temposExecucao98495.txt");
+file98629 = load("temposExecucao98629.txt");
 
-T = file(:,1);
-P = file(:,2);
-t = log10(file(:,3));
+T98491 = file98491(:,1);
+P98491 = file98491(:,2);
+t98491 = log10(file98491(:,3));
 
-% T2 = file2(:,1);
-% P2 = file2(:,2);
-% t2 = log10(file2(:,3));
-% 
-% T3 = file3(:,1);
-% P3 = file3(:,2);
-% t3 = log10(file3(:,3));
+T98495 = file98495(:,1);
+P98495 = file98495(:,2);
+t98495 = log10(file98495(:,3));
+
+T98629 = file98629(:,1);
+P98629 = file98629(:,2);
+t98629 = log10(file98629(:,3));
 
 figure(1)
-plot3(T, P, t, '-s');
-
-% plot3(T2, P2, t2, '-or');
-% plot3(T3, P3, t3, '-tg');
+plot3(T98491, P98491, t98491, '-s');
+hold on;
+plot3(T98495, P98595, t98495, '-or');
+plot3(T98629, P98629, t98629, '-tg');
+hold off;
 
 title 'Todos os Tempos de Execução'
 xlabel ('Tarefas')
 ylabel ('Programadores')
 zlabel ({'Tempo (s)', '_{(log10)}'});
+legend ('98491', '98495', '98629');
 grid on;
