a880c4b6d5c74294771d50d0c6bac3238516c1f0
TheScorpoi <sobral@ua.pt>
2020-12-28 14:04:17 +0000

    without profit marta

--- Resultados/extract_without_profit.sh
+++ Resultados/extract_without_profit.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 # script bash onde guardamos em 2 ficheiros .txt os tempos de execução, e os profits de cada task
 
-cd ../098491_1/
+cd ../098629_1/
 
-grep "Programing Tasks =" *.txt | sed -e 's/_1.txt:Programing Tasks =//' -e 's/_/ /' >../Resultados/programing98491.txt
-grep "Terminal Cases =" *.txt | sed -e 's/_1.txt:Terminal Cases =//' -e 's/_/ /' >../Resultados/terminal98491.txt
+grep "Programing Tasks =" *.txt | sed -e 's/_1.txt:Programing Tasks =//' -e 's/_/ /' >../Resultados/programing98629.txt
+grep "Terminal Cases =" *.txt | sed -e 's/_1.txt:Terminal Cases =//' -e 's/_/ /' >../Resultados/terminal98629.txt
 
 
