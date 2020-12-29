8e5f579a704f4f44609e9f00e15d61b317648ce9
TheScorpoi <sobral@ua.pt>
2020-12-29 17:13:17 +0000

    histogramas com distribuicao

--- Resultados/MatLab/profitHistograma.m
+++ Resultados/MatLab/profitHistograma.m
@@ -8,21 +8,21 @@ file98629 = load("../totalsProfit98629.txt");
 
 figure(1)
 profit98491 = file98491(:, 3);
-hist(profit98491);
+histfit(profit98491);
 title 'Histograma Profit - 98491'
 xlabel 'Profit'
 ylabel 'Número de ocorrências'
 
 figure(2)
 profit98495 = file98495(:, 3);
-hist(profit98495);
-title 'Histograma Profit -98495'
+histfit(profit98495);
+title 'Histograma Profit - 98495'
 xlabel 'Profit'
 ylabel 'Número de ocorrências'
 
 figure(3)
 profit98629 = file98629(:, 3);
-hist(profit98629);
+histfit(profit98629);
 title 'Histograma Profit - 98629'
 xlabel 'Profit'
 ylabel 'Número de ocorrências'
